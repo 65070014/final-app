715b48e60b0415812f58bfe816ba67fb2b69c183
Suphawit <112681287+suphawit1@users.noreply.github.com>
2025-10-16 01:02:32 +0700

    fix for host

--- components/doctor/doctor_schedule.tsx
+++ components/doctor/doctor_schedule.tsx
@@ -3,9 +3,8 @@
 import { useEffect, useState } from "react"
 import { Card } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
-import { Clock, User, Calendar, HeartPulse, Link } from "lucide-react"
+import { Clock, User, Calendar, HeartPulse} from "lucide-react"
 import { Button } from "@/components/ui/button"
-import NextLink from "next/link"
 
 
 export function DoctorSchedule() {
--- components/doctor/homepage/patient-card.tsx
+++ components/doctor/homepage/patient-card.tsx
@@ -1,5 +1,7 @@
-import { Calendar, ArrowRight } from "lucide-react"
+/* eslint-disable @next/next/no-img-element */
+import { ArrowRight } from "lucide-react"
 import { Button } from "@/components/ui/button" // สมมติว่ามีการใช้ Button component จาก shadcn/ui
+import Link from "next/link"
 
 type Props = {
   name: string
