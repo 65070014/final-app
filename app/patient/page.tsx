45a34610ff6dd8af22bd914347b612594d357d15
Suphawit <112681287+suphawit1@users.noreply.github.com>
2025-10-16 02:50:35 +0700

    little_fix

--- app/patient/page.tsx
+++ app/patient/page.tsx
@@ -4,7 +4,7 @@ import { HistorySection } from "@/components/patient/dashboard/patient_history"
 
 export default function PatientDashboard() {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-950 p-8">
       <div className="max-w-6xl mx-auto p-4 space-y-6">
 
         <PatientHeader />
